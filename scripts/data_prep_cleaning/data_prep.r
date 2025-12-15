5503c900c5eb52916fb570b921b5519745d23c5d
Ian Becker <ianbeck018@gmail.com>
2025-12-15 14:21:12 -0600

    initial commit new data prep

---
+++ scripts/data_prep_cleaning/data_prep.r
@@ -0,0 +1,16 @@
+##############################
+#
+# Data prep
+# 12/15/2025
+# Ian Becker
+#
+##############################
+
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(sf)
+library(terra)
+library(tigris)
+
+options(tigris_use_cache = TRUE)
